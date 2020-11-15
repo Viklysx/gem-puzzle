25f8990b3b43daeb9f9717d7c19671df8370b759
Viklysx <vikbelec111@yandex.ru>
2020-11-15 03:25:28 +0300

    feat: change the logic of the decision

---
+++ gem-puzzle/src/components/CheckButtons.js
@@ -0,0 +1,17 @@
+import PlayingField from './PlayingField';
+
+class CheckButtons {
+    constructor() {}
+
+    // stirringBack(massMix) {
+    //     const playing = new PlayingField();
+    //     massMix = massMix.reverse();
+    //     for (let i = 0; i < massMix.length; i++) {
+    //         setTimeout(() => {
+    //             playing.move(massMix[i]);
+    //         }, 100 * (i + 0.2));
+    //     }
+    // }
+}
+
+export default CheckButtons;
--- gem-puzzle/src/components/PlayingField.js
+++ gem-puzzle/src/components/PlayingField.js
@@ -1,19 +1,23 @@
+import CheckButtons from './CheckButtons';
+
 class PlayingField {
     constructor() {
         this.wrapper = document.querySelector('#root');
         this.cellSize = 100;
         this.empty = { // пустая ячейка
             value: 0,
-            top: 0,
-            left: 0
+            top: 3,
+            left: 3
         }
         this.cells = [];
+        this.massMix = [];
         this.cells.push(this.empty); // здесь храним информацию о клетках
         this.startCombination();
     }
 
     move(index) {
-        const cell = this.cells[index];
+        const cell = this.cells[index + 1]; // перемещаемый элементы
+
         const leftDiff = Math.abs(this.empty.left - cell.left);
         const topDiff = Math.abs(this.empty.top - cell.top);
 
@@ -29,39 +33,19 @@ class PlayingField {
 
         this.empty.left = cell.left; // координаты текущей ячейки
         this.empty.top = cell.top;
-        
 
         cell.left = emptyLeft;
         cell.top = emptyTop;
-
-        let caseСounter = 0;
-        this.cells.forEach((keys) => {
-            if (keys.value > 0 && keys.value < 5) {
-                if (keys.top === 0 && keys.left == keys.value - 1) caseСounter++;
-            }
-            if (keys.value > 4 && keys.value < 9) {
-                if (keys.top === 1 && keys.left == keys.value - 5) caseСounter++;
-            }
-            if (keys.value > 8 && keys.value < 13) {
-                if (keys.top == 2 && keys.left == keys.value - 9) caseСounter++;
-            }
-            if (keys.value > 12 && keys.value < 16) {
-                if (keys.top == 3 && keys.left == keys.value - 13) caseСounter++;
-            }
-        })
-
-        if (caseСounter == 15) alert('ура!');
     }
 
     startCombination() {
-        const numbers = [...Array(15).keys()]
-            .sort(() => Math.random() - 0.5);
+        const numbers = Array.from(Array(16).keys());
         const combination = [];
         let value;
 
         for (let i = 1; i <= 15; i++) {
-            value = numbers[i - 1] + 1;
-            combination.push(value);
+            value = numbers[i];
+            combination.push(value); // выигрышное расположение элементов
         }
         // проверяем комбинацию на решаемость
         let z = 0;
@@ -72,45 +56,43 @@ class PlayingField {
                 }
             }
         }
-        z = z + 1; // прибавили номер ряда пустой ячейки
-        if (z % 2 === 0) {
-            let indexImg;
-            for (let i = 0; i < 15; i++) {
-                const cell = document.createElement('div');
+        let indexImg;
+        for (let i = 0; i < 15; i++) {
+            const cell = document.createElement('div');
 
-                cell.className = 'cell';               
-                cell.innerHTML = `<span>${combination[i]}</span>`;
-                indexImg = combination[0];
+            cell.className = 'cell';
+            cell.innerHTML = `<span>${combination[i]}</span>`;
+            indexImg = combination[0];
 
-                const left = (i + 1) % 4; // позиция ячейки, считая слева
-                const top = ((i + 1) - left) / 4;
+            const left = (i) % 4; // позиция ячейки, считая слева
+            const top = ((i) - left) / 4;
 
-                const leftImg = (combination[i]-1) % 4; // позиция ячейки, считая слева
-                const topImg = ((combination[i]-1) - leftImg) / 4;
+            const leftImg = (combination[i] - 1) % 4; // позиция ячейки, считая слева
+            const topImg = ((combination[i] - 1) - leftImg) / 4;
 
-                this.cells.push({
-                    value: combination[i],
-                    left: left,
-                    top: top,
-                    element: cell
-                })
-
-                cell.style.top = `${top*this.cellSize + 100}px`;
-                cell.style.left = `${left*this.cellSize}px`;
-                cell.style['background-position'] = `calc((100% / 3) * ${leftImg}) calc((100% / 3) * ${topImg})`;
-                this.wrapper.append(cell);
-
-                cell.addEventListener('click', () => {
-                    this.move(i + 1);
-                })
-            }
-
-            let elements =  document.querySelectorAll('.cell');
-            elements.forEach(key => {
-                key.style.backgroundImage = `url(./img/${indexImg}.jpg)`;
+            this.cells.push({
+                value: combination[i],
+                left: left,
+                top: top,
+                element: cell
             })
-            this.addButtons();
-        } else this.startCombination();
+
+            cell.style.top = `${top*this.cellSize + 100}px`;
+            cell.style.left = `${left*this.cellSize}px`;
+            cell.style['background-position'] = `calc((100% / 3) * ${leftImg}) calc((100% / 3) * ${topImg})`;
+            this.wrapper.append(cell);
+
+            cell.addEventListener('click', () => {
+                this.move(i);
+            })
+        }
+
+        let elements = document.querySelectorAll('.cell');
+        elements.forEach(key => {
+            key.style.backgroundImage = `url(./img/${indexImg}.jpg)`;
+        })
+        this.addButtons();
+        this.stirring();
     }
 
     createElements(options) {
@@ -120,21 +102,63 @@ class PlayingField {
     }
 
     addButtons() {
-        const wrapButtons = this.createElements({node: 'div', class: 'root-elements'});
-        const newGame = this.createElements({node: 'button', class: 'new-game'});
-        const reset = this.createElements({node: 'button', class: 'reset'});
-        const sound = this.createElements({node: 'button', class: 'new-game'});
-        const resolve = this.createElements({node: 'button', class: 'resolve'});
-        const viewNumbers = this.createElements({node: 'button', class: 'resolve'});
-        const rating = this.createElements({node: 'button', class: 'reset'});
+        const wrapButtons = this.createElements({
+            node: 'div',
+            class: 'root-elements'
+        });
+        const newGame = this.createElements({
+            node: 'button',
+            class: 'new-game'
+        });
+        const reset = this.createElements({
+            node: 'button',
+            class: 'reset'
+        });
+        const sound = this.createElements({
+            node: 'button',
+            class: 'sound'
+        });
+        const resolve = this.createElements({
+            node: 'button',
+            class: 'resolve'
+        });
+        const viewNumbers = this.createElements({
+            node: 'button',
+            class: 'numbers'
+        });
+        const rating = this.createElements({
+            node: 'button',
+            class: 'rating'
+        });
         newGame.textContent = 'Новая игра';
         sound.textContent = 'Вкл/выкл звук';
         reset.textContent = 'Сбросить';
         resolve.textContent = 'Решение';
         viewNumbers.textContent = 'Скрыть/показать цифры';
         rating.textContent = 'Топ 10';
-        wrapButtons.append(newGame, reset, resolve, viewNumbers, rating, sound); 
+        wrapButtons.append(newGame, reset, resolve, viewNumbers, rating, sound);
         this.wrapper.append(wrapButtons);
+
+        resolve.addEventListener('click', () => {
+            this.stirringBack(this.massMix);
+        })
+    }
+
+    stirring() {
+        for (let i = 1; i <= 300; i++) {
+            let randomNumber = (Math.floor(Math.random() * 15));
+            this.move(randomNumber);
+            this.massMix.push(randomNumber);
+        }     
+    } 
+    
+    stirringBack(massMix) {
+        massMix = massMix.reverse();
+        for (let i = 0; i < massMix.length; i++) {
+            setTimeout(() => {
+                this.move(massMix[i]);
+            }, 100 * (i + 0.2));
+        }
     }
 }
 
